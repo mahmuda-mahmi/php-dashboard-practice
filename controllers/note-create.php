2bfa06f61b137e9970d76348d7e20debf35f49cc
Mahmuda Akter <mahmudapakter@gmail.com>
2026-08-27 17:59:26 +0600

    done for today

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,10 +7,22 @@ $db = new Database($config['database']);
 $heading = "Create a note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    $errors = [];
+
+    if(strlen($_POST['body']) === 0) {
+        $errors['body'] = 'Body is required';
+    }
+
+    if(strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Too many characters, max 1000';
+    }
+
+    if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
             'body' => $_POST['body'],
             'user_id' => 1
-    ]);
+        ]);
+    } 
 }
 
 
--- view/note-create.view.php
+++ view/note-create.view.php
@@ -16,7 +16,17 @@
                             <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
                             
                             <div class="mt-2">
-                                <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Write your daily notes"></textarea>
+                                <textarea 
+                                    id="body" 
+                                    name="body" 
+                                    rows="3" 
+                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" 
+                                    placeholder="Write your daily notes"
+                                    ><?= $_POST['body'] ?? '' ?></textarea>
+                    
+                                    <?php if (isset($errors['body'])) : ?>
+                                        <p class="text-red-500 text-sm"><?= $errors['body'] ?></p>
+                                    <?php endif; ?>
                             </div>
                         </div>
 
